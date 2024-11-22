61babf98dfaed07d21b736fa8baf6c50ace577f3
Mhanaj1 <marwan.hanaj@gmail.com>
2024-11-22 05:08:17 +0300

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         XHamster User Search (Fixed)
+// @name         XHamster User Search (Original Working Version)
 // @version      1.0
 // @description  Adds search functionality
 // @match        https://xhamster.com/videos/*
@@ -9,7 +9,7 @@
 (function() {
     'use strict';
 
-    // Desktop elements (using exact working code)
+    // Desktop Elements
     var desktopElements = document.querySelectorAll('.video-page .body-8643e.label-5984a.label-96c3e');
     desktopElements.forEach(function(element) {
         var searchButton = document.createElement('button');
@@ -22,7 +22,7 @@
         searchButton.style.cursor = 'pointer';
         searchButton.style.display = 'flex';
         searchButton.style.alignItems = 'center';
-
+        searchButton.innerHTML = '<i class="fa fa-search" style="margin-right: 5px;"></i> <img src="https://duckduckgo.com/favicon.ico" width="16" height="16">';
         searchButton.onmousedown = function(event) {
             if (event.button === 0) { // Left click
                 var url = 'https://duckduckgo.com/?q=' + encodeURIComponent(element.textContent.trim() + ' site:xhamster.com') + '&ia=web';
@@ -32,11 +32,10 @@
                 window.open(url, '_blank');
             }
         };
-
         element.parentNode.appendChild(searchButton);
     });
 
-    // Mobile elements (using exact same code, just different selector)
+    // Mobile Elements (exact same code)
     var mobileElements = document.querySelectorAll('.subscribe-block__name');
     mobileElements.forEach(function(element) {
         var searchButton = document.createElement('button');
@@ -49,7 +48,7 @@
         searchButton.style.cursor = 'pointer';
         searchButton.style.display = 'flex';
         searchButton.style.alignItems = 'center';
-
+        searchButton.innerHTML = '<i class="fa fa-search" style="margin-right: 5px;"></i> <img src="https://duckduckgo.com/favicon.ico" width="16" height="16">';
         searchButton.onmousedown = function(event) {
             if (event.button === 0) { // Left click
                 var url = 'https://duckduckgo.com/?q=' + encodeURIComponent(element.textContent.trim() + ' site:xhamster.com') + '&ia=web';
@@ -59,7 +58,13 @@
                 window.open(url, '_blank');
             }
         };
-
         element.parentNode.appendChild(searchButton);
     });
+
+    // Original style addition
+    var style = document.createElement('style');
+    style.innerHTML = `
+        @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
+    `;
+    document.head.appendChild(style);
 })();
